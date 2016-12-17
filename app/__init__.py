a03f81f840d3c62b799b523227a98cd07c05e08e
Sisoma Gmo Munden <sisomagmomunden@gmail.com>
2016-12-17 23:01:09 +0000

    not final

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, send_from_directory
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/datasets/<string:dataset_file>")
+def datasets(dataset_file):
+	return send_from_directory('static', "sample_datasets/" + dataset_file)
 
 if __name__ == "__main__":
     app.run()
